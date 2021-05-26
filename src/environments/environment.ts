d30c3fcbb49ed1196db58d1de66ea61b3c997a7b
husainsaqib56 <husainsaqib56@gmail.com>
2021-05-26 15:43:36 +0530

    auth-app

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+// src/environments/environment.ts
+
+import { domain, clientId } from '../../auth_config.json';
+
+export const environment = {
+  production: false,
+  auth: {
+    domain,
+    clientId,
+    redirectUri: window.location.origin,
+  },
+  // dev: {
+  //   serverUrl,
+  // },
+};
